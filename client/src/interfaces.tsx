8ef4439afdd1cd4b60f46a4bb620e02b0b77bb10
YH <yaoyiheng@google.com>
2020-06-03 18:19:43 +0800

    Added PlacesAPI support, setup API/OAuth2.0 keys in GCP project

--- client/src/interfaces.tsx
+++ client/src/interfaces.tsx
@@ -37,3 +37,34 @@ export const emptyStore = (): Store => ({
   latitude: 0.0,
   longitude: 0.0,
 });
+
+export interface IdentityToken {
+  iss: string;
+  sub: string;
+  aud: string;
+  iat: number;
+  exp: number;
+}
+
+export const emptyIdentityToken = (): IdentityToken => ({
+  iss: "",
+  sub: "EMPTY",
+  aud: "",
+  iat: -1,
+  exp: 0,
+});
+
+export interface GMapPlace {
+  business_status: string;
+  geometry: {
+    location: {
+      lat: number;
+      lng: number;
+    };
+  };
+  icon: string;
+  id: string;
+  name: string;
+  place_id: string;
+  vicinity: string;
+}
