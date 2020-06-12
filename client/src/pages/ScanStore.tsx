9af0fec128488a5b0d2d7a2943abab174ac7176d
redhoteggplant <nadiai@google.com>
2020-06-12 23:30:51 +0800

    remove shoppingList and idxMap

--- client/src/pages/ScanStore.tsx
+++ client/src/pages/ScanStore.tsx
@@ -41,63 +41,23 @@ function ScanStore() {
   const merchantID = urlParams.get("mid");
 
   const [showCart, setShowCart] = useState(false);
-  const [shoppingList, setShoppingList] = useState<CartItem[]>([]);
-  //DEBUGGING Current barcode to 'scan'
   const [curBarcode, setCurBarcode] = useState<string>("");
-
-  // Uploaded image data
   const [uploadImg, setUploadImg] = useState<MediaResponse>(emptyMediaResponse);
 
-  //TODO: Look into `class ScanStore extends React.Component` syntax
-  //      in order to declare class-level instance variables (cache cart)
-  let cartItems: CartItem[] = [];
-  // Build barcode:idx mapping
-  let idxMap: { [key: string]: number } = {};
-
-  // wrapper function to fetch cart items and execute callback upon success
-  const fetchCart = async () => {
-    let data = {
-      "store-id": storeID,
-    };
-    const cart = await fetchJson(data, "/api/cart");
-    const items = await fetchCartItems(cart);
-    updateShoppingList(cartItems, items);
-  };
-
-  const fetchCartItems = async (items: any) => {
-    // Empty cart contents
-    cartItems.length = 0;
-    // replace with current items
-    for (let i = 0; i < items.length; ++i) {
-      cartItems.push(items[i]);
-    }
-    // Should do batched fetch with list of barcodes in 1 request-response
-    const itemBarcodes = items.map((zippedItem: any) => zippedItem.barcode);
-    let data = {
-      //'merchant-id': merchantID,
-      "store-id": storeID,
-      items: itemBarcodes,
-    };
-    return fetchJson(data, "/api/items");
-  };
-
-  const updateShoppingList = (items: any, extractedItems: any) => {
-    const sList = items.map((zippedItem: any) => {
-      let cItem: CartItem = {
-        item: extractedItems[zippedItem.barcode],
-        quantity: zippedItem.quantity
-      };
-      return cItem;
-    });
-    setShoppingList(sList);
-  };
+  // const emptyCart = new Map<string, CartItem>();
+  // const [cart, setCart] = useState<Map<string, CartItem>>(emptyCart);
+  
+  const [cartItems, updateCart] = useState<CartItem[]>([]);
 
   const setDebugItem = (barcode: string) => {
     setCurBarcode(barcode);
-    setShoppingList(cartItems);
+    // updateCart(cartItems);
   };
 
   const addItem = async () => {
+    if (curBarcode == "") { // DEBUG: remove
+      await setCurBarcode("93245036");  
+    }
     if (curBarcode == "") {
       // If empty, use media API
       const imgReq = {
@@ -109,68 +69,49 @@ function ScanStore() {
         .then((res: any) => res);
       setUploadImg(imgRes);
     } else {
-      let barcodes: string[] = [curBarcode];
-      fetchItem(barcodes);
+      await addItemToCart(curBarcode);
     }
   };
 
-  const fetchItem = async (barcodes: string[]) => {
-    // Check first if we have previously retrieved this item
-    let unknown_barcodes: string[] = [];
-    let matchedItems: Item[] = [];
-    for (let i = 0; i < barcodes.length; ++i) {
-      if (idxMap[barcodes[i]] != undefined) {
-        let matchedCartItem = cartItems[idxMap[barcodes[i]]];
-        matchedItems.push(matchedCartItem.item);
-      } else {
-        unknown_barcodes.push(barcodes[i]);
-      }
-    }
-    if (matchedItems.length > 0) {
-      displayItems(matchedItems);
-    } else if (unknown_barcodes.length > 0) {
-      let data = {
-        "merchant-id": merchantID,
-        barcode: unknown_barcodes,
-      };
-      const items = await fetchJson(data, "/api/items");
-      displayItems(items);
-    }
-  };
-
-  const displayItems = (extractedItems: Item[]) => {
-    for (let i = 0; i < extractedItems.length; ++i) {
-      if (idxMap[extractedItems[i].barcode] != undefined) {
-        cartItems[idxMap[extractedItems[i].barcode]].quantity += 1;
-      } else {
-        let newItem: CartItem = {
-          item: extractedItems[i],
-          quantity: 1
-        };
-        cartItems.push(newItem);
-      }
-    }
-    setShoppingList(cartItems);
-  };
-
-  const updateShoppingListQuantity = (idx: number, quantity: number) => {
-    if (quantity <= 0) {
-      cartItems.splice(idx, 1);
+  const addItemToCart = async (barcode: string) => {
+    const idx = cartItems.findIndex(cartItem => cartItem.item.barcode == barcode);
+    if (idx != -1) {
+      // updateCart(cartItems.filter(cartItem => {
+      //   cartItem.item.barcode !== barcode
+      // }));
+      updateQuantity(idx, cartItems[idx].quantity + 1);
     } else {
-      cartItems[idx].quantity = quantity;
+      const data = {
+        "merchant-id": merchantID,
+        barcode: [barcode]
+      };
+      const [ item ]: Item[] = await fetchJson(data, "/api/items");
+      const cartItem: CartItem = {
+        item: item,
+        quantity: 1
+      }
+      updateCart(cartItems => [...cartItems, cartItem]);
     }
-    setShoppingList(cartItems);
+  };
+
+  const updateQuantity = (idx: number, quantity: number) => {
+    if (quantity <= 0) {
+      updateCart(cartItems.filter((_, i) => i !== idx));
+    } else {
+      const cartItem = cartItems[idx];
+      updateCart(cartItems => ({...cartItems, [idx]: {...cartItem, quantity}}))
+    }
   };
 
   const renderShoppingList = () => {
     let UI_List = [];
-    for (let i = 0; i < shoppingList.length; ++i) {
+    for (let i = 0; i < cartItems.length; ++i) {
       UI_List.push(
         <Grid key={"grid" + i} item xs={12}>
           <ItemCard
-            cartItem={shoppingList[i]}
+            cartItem={cartItems[i]}
             idx={i}
-            updateItemQuantity={updateShoppingListQuantity}
+            updateItemQuantity={updateQuantity}
           />
         </Grid>
       );
@@ -182,20 +123,11 @@ function ScanStore() {
     setShowCart(!showCart);
   };
 
-  const updateIdxMapping = () => {
-    // Retrieve from State first
-    for (let i = 0; i < shoppingList.length; ++i) {
-      cartItems.push(shoppingList[i]);
-      const barcode: string = shoppingList[i].item.barcode;
-      idxMap[barcode] = i;
-    }
-  };
-
   const makePayment = () => {
     //Yiheng: This should be just an ID without exposing the contents of our Order
     window.location.href =
       "/receipt?id=TEST_ORDER&contents=" +
-      encodeURIComponent(JSON.stringify(shoppingList));
+      encodeURIComponent(JSON.stringify(cartItems));
   };
 
   const processImageBarcode = async (img: HTMLImageElement) => {
@@ -220,8 +152,6 @@ function ScanStore() {
     processImageBarcode(img);
   };
 
-  //TODO When we update shoppingList, we should send update to server?
-
   // When we change what image we uploaded, run extract barcode client-side
   useEffect(() => {
     if (uploadImg.mimeType) {
@@ -230,13 +160,6 @@ function ScanStore() {
       processImageBarcode(img);
     }
   }, [uploadImg]);
-
-  // After rerendering of UI and React popping variables off our stack,
-  // reinitialize vars
-  // Also, race? Will this run before all other useEffects? What's the ordering of execution here?
-  useEffect(() => {
-    updateIdxMapping(); //TODO(#20) Yiheng: Tech Debt, we need to make this persist as Class Variable
-  });
 
   return (
     <Container disableGutters={true} className="ScanStore">
@@ -257,11 +180,11 @@ function ScanStore() {
                   <Grid item xs={6}>
                     <TextInputField
                       text={curBarcode ? curBarcode : "...barcode"}
-                      setState={setDebugItem}
+                      setState={setCurBarcode}
                     />
                   </Grid>
                 </Grid>
-                {shoppingList.length > 0 && (
+                {cartItems.length > 0 && (
                   <Grid
                     container
                     spacing={1}
@@ -318,6 +241,10 @@ function ScanStore() {
           </Grid>
         )}
       </Grid>
+      {showCart && [
+        <h3>Current Cart Contents:</h3>,
+        <Cart contents={cartItems} />,
+      ]}
       <Fab
         style={{ position: "fixed", bottom: "10px", left: "10px" }}
         color="primary"
@@ -325,26 +252,13 @@ function ScanStore() {
       >
         {showCart ? <EditIcon /> : <ShoppingCartIcon />}
       </Fab>
-      {showCart && [
-        <h3>Current Cart Contents:</h3>,
-        <Cart contents={shoppingList} />,
-        <Fab
-          style={{ position: "fixed", bottom: "10px", right: "10px" }}
-          color="secondary"
-          onClick={makePayment}
-        >
-          <PaymentIcon />
-        </Fab>,
-      ]}
-      {!showCart && (
-        <Fab
-          style={{ position: "fixed", bottom: "10px", right: "10px" }}
-          color="secondary"
-          onClick={addItem}
-        >
-          <AddIcon />
-        </Fab>
-      )}
+      <Fab
+        style={{ position: "fixed", bottom: "10px", right: "10px" }}
+        color="secondary"
+        onClick={makePayment}
+      >
+        {showCart ? <PaymentIcon /> : <AddIcon />}
+      </Fab>,
     </Container>
   );
 }
