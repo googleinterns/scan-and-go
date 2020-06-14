fa32b78d029e16a2a6642ee934646e15c4732304
redhoteggplant <nadiai@google.com>
2020-06-14 17:17:18 +0800

    constants file

---
+++ client/src/constants/index.tsx
@@ -0,0 +1,27 @@
+// API endpoints
+export const API = '/api';
+export const CART_API = '/api/cart';
+export const ITEMS_API = '/api/items';
+export const ORDER_API = '/api/order';
+export const ORDERS_API = '/api/orders';
+export const PAY_API = '/api/pay';
+export const STORE_API = '/api/store';
+export const STORES_API = '/api/stores';
+export const USERS_API = '/api/users';
+export const USERS_ALL_API = '/api/users/all';
+
+// Data formats
+export const PRICE_FRACTION_DIGITS = 2;
+export const LOCATION_RADIUS_METERS = '5000';
+
+// UI attributes
+export const BARCODE_PLACEHOLDER = '...barcode';
+export const ID_PLACEHOLDER = '...id';
+
+// Default values
+export const DEFAULT_INPUT_TYPE = 'text';
+export const DEFAULT_WELCOME_MSG = 'hello';
+export const DEFAULT_USERNAME = 'commander';
+
+// Test variables
+export const TEST_STORE_ID = 'WPANCUD';
