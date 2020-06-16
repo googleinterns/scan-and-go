b441344b1db8f79d5bfc466578c9abc0bd863e01
devYaoYH <yaoyiheng@gmail.com>
2020-06-16 15:54:17 +0800

    Added testing scripts and framework to Server

--- server/config.js
+++ server/config.js
@@ -1,10 +1,12 @@
 // NODE_ENV strings
 const DEV = "development";
 const PROD = "production";
+const TEST = "test";
 
 module.exports = {
   DEV: DEV,
   PROD: PROD,
+  TEST: TEST,
   ENV: process.env.NODE_ENV || DEV,
   DEV_PORT: 3143,
 };
