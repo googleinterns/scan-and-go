c3a79f14226194e3697a5d6240d17a59f0b8f59d
redhoteggplant <nadiai@google.com>
2020-06-17 09:19:59 +0800

    test render non-empty cart

--- client/src/tests/Cart.test.tsx
+++ client/src/tests/Cart.test.tsx
@@ -1,11 +1,36 @@
 import React from "react";
 import renderer from "react-test-renderer";
+import Enzyme from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
 import Cart from "../components/Cart";
-import { CartItem, emptyCartItem } from "../interfaces";
+import { CartItem } from "../interfaces";
+import { TableBody, TableRow, TableCell } from "@material-ui/core";
 
-it("Cart renders correctly", () => {
-  let state: string = "";
-  const contents: CartItem[] = [emptyCartItem];
+Enzyme.configure({ adapter: new Adapter() });
+
+it("Cart renders correctly when empty", () => {
+  const contents: CartItem[] = [];
   const tree = renderer.create(<Cart contents={contents} />).toJSON();
   expect(tree).toMatchSnapshot();
+});
+
+it("Cart renders correctly with one item", () => {
+  const contents: CartItem[] = [
+    {
+      item: {
+        name: "cheese",
+        price: 3.2,
+        "merchant-id": "WPANCUD",
+        barcode: "93245036",
+      },
+      quantity: 2,
+    },
+  ];
+  const wrapper = Enzyme.shallow(<Cart contents={contents} />);
+  const rows = wrapper.find(TableBody).find(TableRow);
+  expect(rows).toHaveLength(2);
+  const itemPrice = rows.last().find(TableCell).last();
+  expect(itemPrice.text()).toEqual("6.40");
+  const totalPrice = rows.last().find(TableCell).last();
+  expect(totalPrice.text()).toEqual("6.40");
 });
