3e15d38b1e8a02acc204d713d707fa8701a9f919
redhoteggplant <nadiai@google.com>
2020-06-17 09:26:33 +0800

    minor edits to address PR comments

--- client/src/actions/Cart.actions.tsx
+++ client/src/actions/Cart.actions.tsx
@@ -2,11 +2,11 @@ import { CartItem } from "./../interfaces";
 import { PRICE_FRACTION_DIGITS } from "../constants";
 
 export const getTotalPrice = (cartItems: CartItem[]) => {
-  let tot_price = 0.0;
-  for (let cartItem of cartItems) {
-    tot_price += cartItem.quantity * cartItem.item.price;
+  let totPrice = 0.0;
+  for (const cartItem of cartItems) {
+    totPrice += cartItem.quantity * cartItem.item.price;
   }
-  return tot_price.toFixed(PRICE_FRACTION_DIGITS);
+  return totPrice.toFixed(PRICE_FRACTION_DIGITS);
 };
 
 export const getSubtotalPrice = (cartItem: CartItem) => {
