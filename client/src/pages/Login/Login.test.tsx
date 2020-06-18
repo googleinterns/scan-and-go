b8a357121f2423edf1b65b99e071401d918bd365
devYaoYH <yaoyiheng@gmail.com>
2020-06-18 19:32:49 +0800

    Resolved comments in PR #90

--- client/src/pages/Login/Login.test.tsx
+++ client/src/pages/Login/Login.test.tsx
@@ -5,8 +5,8 @@ import Enzyme from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 import Login from "./Login";
 import { Input } from "@material-ui/core";
-import * as constants from "./../constants/index";
-import { isWeb, microapps } from "./../constants/index";
+import * as config from "./../config";
+import { isWeb, microapps } from "./../config";
 
 Enzyme.configure({ adapter: new Adapter() });
 
@@ -15,44 +15,58 @@ Object.defineProperty(window, "location", {
   value: { assign: jest.fn() },
 });
 
-it("Login Page renders web UI correctly", () => {
-  const tree = renderer.create(<Login />).toJSON();
-  expect(tree).toMatchSnapshot();
+describe("Test on Web UI", () => {
+  const origIsWeb = config.isWeb;
+
+  beforeAll(() => {
+    config.isWeb = true;
+  });
+
+  afterAll(() => {
+    config.isWeb = origIsWeb;
+  });
+
+  it("Login Page renders web UI correctly", () => {
+    const tree = renderer.create(<Login />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it("Login Page redirects on web given valid user", () => {
+    const validUser = "Admin";
+    const mountedWrapper = Enzyme.mount(<Login />);
+    const input = mountedWrapper.find("#username").last();
+    input.simulate("blur", { target: { value: validUser } });
+    expect(window.location.assign).toBeCalledWith("/home");
+  });
 });
 
-it("Login Page redirects on web given valid user", () => {
-  const validUser = "Admin";
-  const mountedWrapper = Enzyme.mount(<Login />);
-  const input = mountedWrapper.find("#username").last();
-  input.simulate("blur", { target: { value: validUser } });
-  expect(window.location.assign).toBeCalledWith("/home");
-});
+describe("Simulate test within GPay Env", () => {
+  const origIsWeb = config.isWeb;
+  const origMicroapps = config.microapps;
 
-it("Login Page renders microapps UI correctly", () => {
-  // Set isWeb to true to render web UI
-  const origIsWeb = constants.isWeb;
-  constants.isWeb = false;
-  const tree = renderer.create(<Login />).toJSON();
-  expect(tree).toMatchSnapshot();
-  constants.isWeb = origIsWeb;
-});
+  beforeAll(() => {
+    config.isWeb = false;
+    config.microapps = global.microapps;
+  });
 
-it("Login Page calls microapps Identity API to login", async () => {
-  // Interject our definitions for constant variables
-  const origIsWeb = constants.isWeb;
-  constants.isWeb = false;
-  const origMicroapps = constants.microapps;
-  constants.microapps = global.microapps;
-  // We should automatically call Login function
-  // and interact with microapps API on mount
-  const microappsIdentityAPI = jest.spyOn(global.microapps, "getIdentity");
-  // Mount our Component after mocking API call
-  const mountedWrapper = Enzyme.mount(<Login />);
-  expect(microappsIdentityAPI).toHaveBeenCalledTimes(1);
-  // After Login is called and we sent over a valid identity
-  // we should expect a url redirect
-  await waitFor(() => expect(window.location.assign).toBeCalledWith("/home"));
-  // Reinstate our original global values to avoid side effects
-  constants.isWeb = origIsWeb;
-  constants.microapps = origMicroapps;
+  afterAll(() => {
+    config.isWeb = origIsWeb;
+    config.microapps = origMicroapps;
+  });
+
+  it("Login Page renders microapps UI correctly", async () => {
+    const tree = renderer.create(<Login />).toJSON();
+    await waitFor(() => expect(tree).toMatchSnapshot());
+  });
+
+  it("Login Page calls microapps Identity API to login", async () => {
+    // We should automatically call Login function
+    // and interact with microapps API on mount
+    const microappsIdentityAPI = jest.spyOn(global.microapps, "getIdentity");
+    const mountedWrapper = Enzyme.mount(<Login />);
+    expect(microappsIdentityAPI).toHaveBeenCalledTimes(1);
+    // After Login is called and we sent over a valid identity
+    // we should expect a url redirect
+    await waitFor(() => expect(window.location.assign).toBeCalledWith("/home"));
+  });
 });
