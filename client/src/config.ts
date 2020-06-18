f3dac1590e19f4a1ef750f2e929fbcbe7cd1e2b8
devYaoYH <yaoyiheng@gmail.com>
2020-06-18 19:32:49 +0800

    Resolved comments in PR #90

---
+++ client/src/config.ts
@@ -0,0 +1,15 @@
+declare const window: any;
+
+export const isDebug = false;
+
+// Grab a handle to our microapps javascript library
+export const microapps = window.microapps;
+
+// Grab handle to google client js API
+export const google = window.google;
+
+// Detect if our app is running from within an iframe
+// On a normal site, the window.parent variable will
+// reference itself. However, within the microapps environment
+// window will be the iframe and window.parent will not be visible
+export const isWeb = window === window.parent;
