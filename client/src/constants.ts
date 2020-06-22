bb1222f209e89da05af7ab44ec098c9a5b424cd8
devYaoYH <yaoyiheng@gmail.com>
2020-06-22 16:46:50 +0800

    Replaced string for StoreHeader link in ScanStore with a Constant

--- client/src/constants.ts
+++ client/src/constants.ts
@@ -1,3 +1,8 @@
+// Client Site Pages
+export const HOME_PAGE = "/home";
+export const SCANSTORE_PAGE = "/store";
+export const RECEIPT_PAGE = "/receipt";
+export const LOGIN_PAGE = "/";
 // API endpoints
 export const API = "/api/";
 export const CART_API = "/api/cart";
