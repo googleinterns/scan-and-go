41498f4318552d72d4013bcfac439d324420d6d9
redhoteggplant <nadiai@google.com>
2020-06-24 16:57:56 +0800

    replace global setup with populate and clear db between tests

--- server/tests/routes.test.js
+++ server/tests/routes.test.js
@@ -5,6 +5,49 @@ const TEST_USERS = require("./../data/users.json");
 const TEST_ITEMS = require("./../data/items.json");
 const TEST_STORES = require("./../data/stores.json");
 const TEST_ORDERS = require("./../data/orders.json");
+const {
+  usersCollection,
+  itemsCollection,
+  storesCollection,
+  ordersCollection,
+} = require("./../firestore");
+const { sortUsers, sortItems, sortStores, sortOrders } = require("./testUtils");
+const { populate, clearDB } = require("./../emulatedFirestore.js");
+
+beforeEach(async () => {
+  await populate();
+});
+
+afterEach(async () => {
+  await clearDB();
+});
+
+afterAll(() => {
+  server.close();
+});
+
+describe("Assert Mock Database Loaded", () => {
+  it("Users database should match", async () => {
+    const usersQuery = await usersCollection.get();
+    const users = usersQuery.docs.map((doc) => doc.data());
+    expect(users.sort(sortUsers)).toEqual(TEST_USERS.sort(sortUsers));
+  });
+  it("Items database should match", async () => {
+    const itemsQuery = await itemsCollection.get();
+    const items = itemsQuery.docs.map((doc) => doc.data());
+    expect(items.sort(sortItems)).toEqual(TEST_ITEMS.sort(sortItems));
+  });
+  it("Stores database should match", async () => {
+    const storesQuery = await storesCollection.get();
+    const stores = storesQuery.docs.map((doc) => doc.data());
+    expect(stores.sort(sortStores)).toEqual(TEST_STORES.sort(sortStores));
+  });
+  it("Orders database should match", async () => {
+    const ordersQuery = await ordersCollection.get();
+    const orders = ordersQuery.docs.map((doc) => doc.data());
+    expect(orders.sort(sortOrders)).toEqual(TEST_ORDERS.sort(sortOrders));
+  });
+});
 
 describe("API POST Data", () => {
   it("should get TEST_USER details", async () => {
@@ -68,9 +111,6 @@ describe("API POST Data", () => {
     for (const [i, _] of res.body.entries()) {
       expect(res.body[i]["store-id"]).toEqual(expectedStores[i]["store-id"]);
     }
-    // expect(
-    //   res.body.every((store) => expectedStores.includes(store["store-id"]))
-    // ).toBe(true);
   });
   it("should display list of items given barcodes", async () => {
     const testMerchant = "WPANCUD";
@@ -112,5 +152,3 @@ describe("API GET Data", () => {
     );
   });
 });
-
-server.close();
