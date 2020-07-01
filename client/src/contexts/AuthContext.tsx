a2af029684799ca6e4c27ff8173efefa13422da0
redhoteggplant <nadia.irwanto@yale.edu>
2020-07-01 22:00:46 +0800

    replace custom hook with useReducer for long-term flexibility

--- client/src/contexts/AuthContext.tsx
+++ client/src/contexts/AuthContext.tsx
@@ -1,29 +1,49 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useState, useReducer } from "react";
 import { getUser } from "src/pages/Actions";
 import { User, emptyUser } from "src/interfaces";
 
 export const AuthContext = createContext({
   user: emptyUser,
-  setAuth: (user: User) => {},
-  unsetAuth: () => {}
+  setUser: (user: User) => {},
+  unsetUser: () => {},
 });
+
+const reducer = (state: any, action: any) => {
+  switch (action.type) {
+    case "setUser": {
+      const { user } = action.payload;
+      window.localStorage.setItem("user", JSON.stringify(user));
+      return {
+        ...state,
+        user: user,
+      };
+    }
+    case "unsetUser":
+      window.localStorage.clear();
+      return {
+        ...state,
+        user: emptyUser,
+      };
+  }
+};
 
 // Context provider
 export const AuthContextProvider: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
-  const [user, setUser] = useState(getUser());
-  const setAuth = (user: User) => {
-    window.localStorage.setItem("user", JSON.stringify(user));
-    setUser(user);
+  const initialState = { user: getUser() };
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { user } = state;
+
+  const setUser = (user: User) => {
+    dispatch({ type: "setUser", payload: { user } });
   };
-  const unsetAuth = () => {
-    window.localStorage.clear();
-    setUser(emptyUser);
+  const unsetUser = () => {
+    dispatch({ type: "unsetUser" });
   };
 
   return (
-    <AuthContext.Provider value={{ user, setAuth, unsetAuth }}>
+    <AuthContext.Provider value={{ user, setUser, unsetUser }}>
       {children}
     </AuthContext.Provider>
   );
