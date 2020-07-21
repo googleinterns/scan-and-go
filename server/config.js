bd833851613507368455a5a5cc924ad9ae70340c
devYaoYH <yaoyiheng@gmail.com>
2020-07-21 15:10:21 +0800

    Added pre-specified headers for csv templates

--- server/config.js
+++ server/config.js
@@ -12,4 +12,19 @@ module.exports = {
   CLOUD_BUCKET: "scan-and-go-for-gpay-storage",
   DOWNLOAD_FOLDER: "data",
   MERCHANTS_INGESTION_CSV: "merchants.csv",
+  // Statically defined headers for now
+  // These are internal (server) field keys for objects built
+  // from business-provided .csv files
+  // We understand columns via ordering
+  //TODO(#): File verification via server-side config?
+  MERCHANTS_CSV_HEADERS: [
+    "Merchant ID",
+    "Microapp ID",
+    "payeeVpa",
+    "Name",
+    "Items",
+    "Stores",
+  ],
+  ITEMS_CSV_HEADERS: ["Index", "Barcode", "Name", "Price", "Unit", "Details"],
+  STORES_CSV_HEADERS: ["Index", "Store ID", "Name", "Address"],
 };
