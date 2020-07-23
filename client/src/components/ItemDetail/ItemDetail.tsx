3d4d9f6a8c971ed00d40dc8193c72728ebf759f1
devYaoYH <yaoyiheng@gmail.com>
2020-07-23 19:05:26 +0800

    Added add/remove confirmation flow

--- client/src/components/ItemDetail/ItemDetail.tsx
+++ client/src/components/ItemDetail/ItemDetail.tsx
@@ -2,7 +2,8 @@ import React, { useEffect, useState } from "react";
 import { CartItem, emptyItem } from "src/interfaces";
 import { Button, Typography, Grid, IconButton } from "@material-ui/core";
 import CloseIcon from "@material-ui/icons/Close";
-import { useTheme } from "@material-ui/core/styles";
+import { useTheme, MuiThemeProvider } from "@material-ui/core/styles";
+import { ErrorTheme } from "src/theme";
 import ItemCardQuantityMixer from "src/components/ItemCard/ItemCardQuantityMixer";
 import { PRICE_FRACTION_DIGITS, PLACEHOLDER_ITEM_MEDIA } from "src/constants";
 import { getSubtotalPrice, parseRawTextNewlines } from "src/utils";
@@ -10,21 +11,59 @@ import { getSubtotalPrice, parseRawTextNewlines } from "src/utils";
 function ItemDetail({
   cartItem,
   closeCallback,
-  updateItemQuantity,
+  updateItemCallback,
+  currentCart,
 }: {
   cartItem: CartItem | undefined;
   closeCallback: () => void;
-  updateItemQuantity: (barcode: string, quantity: number) => void;
+  updateItemCallback: (cartItem: CartItem) => void;
+  currentCart?: CartItem[];
 }) {
+  const [curAmount, setCurAmount] = useState<number>(0);
+  const [isRemoving, setIsRemoving] = useState<boolean>(false);
+
   const theme = useTheme();
+
   const item = cartItem ? cartItem.item : emptyItem();
   const cornerRoundingPercentage = 10;
 
+  // Determine if is new item
+  const isNewItem =
+    currentCart &&
+    !currentCart
+      .map((curItem: CartItem) => curItem.item.barcode)
+      .includes(item.barcode);
+
+  // Callback for +/- mixer to update chosen item quantity
   const updateItemQuantityWrapper = (quantity: number) => {
     if (item) {
-      updateItemQuantity(item.barcode, quantity);
+      if (quantity <= 0) {
+        setIsRemoving(true);
+        setCurAmount(0);
+        return;
+      }
+      setIsRemoving(false);
+      setCurAmount(quantity);
     }
   };
+
+  const confirmUpdate = () => {
+    const newItem: CartItem = {
+      item: item,
+      quantity: curAmount,
+    };
+    updateItemCallback(newItem);
+  };
+
+  useEffect(() => {
+    if (cartItem) {
+      setCurAmount(cartItem.quantity);
+    } else {
+      setCurAmount(0);
+    }
+    // Everytime we change to a 'new' item, refresh removal state flag
+    setIsRemoving(false);
+  }, [cartItem]);
 
   return (
     <div style={{ height: "100vh", width: "100%" }}>
@@ -94,8 +133,9 @@ function ItemDetail({
             <Grid item container direction="row" justify="space-between">
               <Grid item style={{ marginBottom: theme.spacing(2) }}>
                 <ItemCardQuantityMixer
-                  quantity={cartItem ? cartItem.quantity : 0}
+                  quantity={curAmount ? curAmount : 0}
                   updateQuantity={updateItemQuantityWrapper}
+                  disabled={!cartItem}
                 />
               </Grid>
               <Grid item xs>
@@ -114,9 +154,28 @@ function ItemDetail({
               </Grid>
             </Grid>
             <Grid item style={{ marginBottom: theme.spacing(1) }}>
-              <Button variant="contained" fullWidth={true} color="primary">
-                Add to Cart
-              </Button>
+              {isRemoving ? (
+                <MuiThemeProvider theme={ErrorTheme}>
+                  <Button
+                    variant="contained"
+                    fullWidth={true}
+                    color="secondary"
+                    onClick={confirmUpdate}
+                  >
+                    Remove Item
+                  </Button>
+                </MuiThemeProvider>
+              ) : (
+                <Button
+                  disabled={!cartItem}
+                  variant="contained"
+                  fullWidth={true}
+                  color="primary"
+                  onClick={confirmUpdate}
+                >
+                  {isNewItem ? "Add Item" : "Confirm Change"}
+                </Button>
+              )}
             </Grid>
           </Grid>
         </div>
